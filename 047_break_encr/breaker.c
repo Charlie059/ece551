d6318754422053f0336e04971feecbebabf93685
xg73 <xg73@duke.edu>
2021-09-22 17:03:54 -0400

    047

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -23,9 +23,56 @@ int breaker(FILE * f) {
     }
   }
 
+  // rm the Top 1
+  counter[max_idx] = 0;
   max_idx += 'a';
 
-  return max_idx - 'e';
+  int vote[3] = {0};
+
+  vote[0] = max_idx - 'e';
+  // Find the Top 2 Char
+
+  max_current = 0;
+  max_idx = 0;
+
+  for (size_t i = 0; i < 26; i++) {
+    if (max_current <= counter[i]) {
+      max_current = counter[i];
+      max_idx = i;
+    }
+  }
+
+  counter[max_idx] = 0;
+  max_idx += 'a';
+  vote[1] = max_idx - 't';
+
+  // Find the Top 3 Char
+
+  max_current = 0;
+  max_idx = 0;
+  // Find the Top 1 Char
+  for (size_t i = 0; i < 26; i++) {
+    if (max_current <= counter[i]) {
+      max_current = counter[i];
+      max_idx = i;
+    }
+  }
+
+  max_idx += 'a';
+  vote[2] = max_idx - 'a';
+
+  if (vote[0] == vote[1]) {
+    return vote[0];
+  }
+  else if (vote[1] == vote[2]) {
+    return vote[1];
+  }
+  else if (vote[0] == vote[2]) {
+    return vote[0];
+  }
+  else {
+    return vote[0];
+  }
 }
 
 int main(int argc, char ** argv) {
