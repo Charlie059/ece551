7df567c75067d387a89858620e44d7c18ddb597d
xg73 <xg73@duke.edu>
2021-09-26 12:10:34 -0400

    Step 1 V1- with population error

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,10 +1,80 @@
 #include "pandemic.h"
 
+#include <errno.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <string.h>  //?
+
+#include "ctype.h"
+#include "stdio.h"
+#include "stdlib.h"
+// Function which print the error message
+void printError(char * error) {
+  fprintf(stderr, "%s", error);
+}
+
 country_t parseLine(char * line) {
-  //WRITE ME
+  const char * readLine = line;  // Get the input String
+
+  const char * ptr_comma = strchr(readLine, ',');  // Check the first comma occurance
+  if (ptr_comma == NULL) {                         //If no comma found, return failure.
+    printError("Expect comma, but nothing found.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Check the length of country name
+  size_t countryLen = 0;
+  countryLen = ptr_comma - readLine;
+  if (countryLen > MAX_NAME_LEN) {  //63?
+    printError("Expect country name size less than 64.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Treat all Strings before the comma (first occurance) as country names
   country_t ans;
-  ans.name[0] = '\0';
-  ans.population = 0;
+
+  //Copy the name part to the ans's name
+
+  for (size_t i = 0; i < countryLen + 1; i++) {
+    if (i != countryLen) {
+      ans.name[i] = readLine[i];
+    }
+    else {
+      ans.name[i] = '\0';
+    }
+  }
+
+  // Work on the population
+
+  const char * ptr_nt = strchr(readLine, '\0');  // Define the null-terminated
+  size_t popLen = ptr_nt - ptr_comma - 1;        // Define the length of population string
+
+  char temp[21] = "";
+  if (popLen > 21) {
+    printError(
+        "Expect country population size less than or equal to 18446744073709551615.\n");
+    exit(EXIT_FAILURE);
+  }
+  //  strncpy(temp, ptr_comma + 1, popLen);  // Copy the population to the temp string
+
+  for (size_t i = 0; i < popLen + 1; i++) {
+    if (i != popLen) {
+      temp[i] = readLine[i + countryLen + 1];
+    }
+    else {
+      temp[i] = '\0';
+    }
+  }
+
+  // Convert temp to uint_64_t
+  uint64_t population = strtoumax(temp, NULL, 10);
+  if (errno == ERANGE) {  // cite man ?
+    printError("Expect population is less than MAX uint_64_t.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  ans.population = population;
+
   return ans;
 }
 
