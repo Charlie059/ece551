7c647a6aa638a553953837b36371a1cd4a22a658
xg73 <xg73@duke.edu>
2021-09-29 01:01:39 -0400

    043

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -14,31 +14,23 @@ void printError(char * error) {
   fprintf(stderr, "%s", error);
 }
 
-// Function which check population value is vaild
-/* void checkVaild(size_t length, char temp[]) { */
-/*   for (size_t i = 0; i < length; i++) { */
-/*     if (isdigit(temp[i]) == 0 && i != 0) { */
-/*       printError("Expect population is all digit.\n"); */
-/*       exit(EXIT_FAILURE); */
-/*     } */
-/*     if (i == 0 && temp[0] != '-') { */
-/*       if (isdigit(temp[0]) == 0) { */
-/*         printError("Expect population is all digit.\n"); */
-/*         exit(EXIT_FAILURE); */
-/*       } */
-/*     } */
-/*   } */
-/* } */
+//Function which check population value is vaild
+void checkVaild(char temp[]) {
+  if (temp[0] != '-' && isdigit(temp[0]) != 1) {
+    printError("Expect population is all digit.\n");
+    exit(EXIT_FAILURE);
+  }
+}
 // Task 1
 country_t parseLine(char * line) {
   country_t ans;
-  const char * readLine = line;                       // Get the input String
-  const char * ptr_comma = strchr(readLine, ',');     // Check the first comma occurance
-  const char * ptr_newline = strchr(readLine, '\n');  // A pointer point to newline
-  const char * ptr_nt = strchr(readLine, '\0');       // Define the null-terminated
-  size_t popLen = ptr_nt - ptr_comma - 1;  // Define the length of population string
-  char temp[21] = "";                      // temp to store the population value
-  size_t countryLen = 0;                   // define the country name length
+  const char * readLine = line;                    // Get the input String
+  const char * ptr_comma = strchr(readLine, ',');  // Check the first comma occurance
+  //  const char * ptr_newline = strchr(readLine, '\n');  // A pointer point to newline
+  const char * ptr_nt = strchr(readLine, '\0');  // Define the null-terminated
+  size_t popLen = ptr_nt - ptr_comma - 1;        // Define the length of population string
+  char temp[21] = "";                            // temp to store the population value
+  size_t countryLen = 0;                         // define the country name length
 
   if (ptr_comma == NULL) {  //If no comma found, return failure.
     printError("Expect comma, but nothing found.\n");
@@ -71,28 +63,7 @@ country_t parseLine(char * line) {
   strncpy(temp, ptr_comma + 1, popLen);  // Copy the population to the temp string
 
   // Check the vaild population
-  if (ptr_newline == NULL) {
-    //    checkVaild(popLen, temp);  // Check if temp has non-value char after first digit
-    if (popLen == 0) {  // Check if no population
-      printError("Expect country population, but not found.\n");
-      exit(EXIT_FAILURE);
-    }
-  }
-  else {                              // If the input contains newline
-    if (ptr_newline + 1 != ptr_nt) {  // If newline is not close to the non-terminal
-      printError("Expect population is all digit, found newline is not last digit "
-                 "(without nonterminal char).\n");
-      exit(EXIT_FAILURE);
-    }
-    else {
-      //     checkVaild(popLen - 1,
-      //         temp);   // Check if temp has non-value char after first digit
-      if (popLen == 1) {  // Check if no population
-        printError("Expect country population, but not found.\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-  }
+  checkVaild(temp);
 
   // Convert temp to uint_64_t
   uint64_t population = strtoumax(temp, NULL, 10);
