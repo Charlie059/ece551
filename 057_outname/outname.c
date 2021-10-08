29f756dd7f9215a2a0ffd0ac9aaf7e01cabb4218
xg73 <xg73@duke.edu>
2021-10-08 00:54:44 -0400

    057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,18 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+
+  const char * extension = ".counts";
+
+  char * new = NULL;
+  new = malloc(strlen(inputName) + 1 + 7);
+
+  strcpy(new, inputName);
+  strcat(new, extension);
+  return new;
 }
