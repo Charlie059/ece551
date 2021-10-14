a5abf7eb90387adf1dc7d75ce699ccfea59d928f
xg73 <xg73@duke.edu>
2021-10-14 15:12:57 -0400

    068

---
+++ 068_vector_ctor/vector.cpp
@@ -0,0 +1,37 @@
+#include "vector.hpp"
+
+#include <cmath>
+#include <cstdio>
+
+/* write your class implementation in this file
+ */
+
+Vector2D::Vector2D() : x(0), y(0) {
+}
+Vector2D::Vector2D(double init_x, double init_y) : x(init_x), y(init_y) {
+}
+
+double Vector2D::getMagnitude() const {
+  return std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+}
+
+Vector2D Vector2D::operator+(const Vector2D & rhs) const {
+  Vector2D ans;
+  ans.x = this->x + rhs.x;
+  ans.y = this->y + rhs.y;
+  return ans;
+}
+
+Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
+  this->x += rhs.x;
+  this->y += rhs.y;
+  return *this;
+}
+
+double Vector2D::dot(const Vector2D & rhs) const {
+  return (this->x * rhs.x + this->y * rhs.y);
+}
+
+void Vector2D::print() const {
+  printf("<%.2f, %.2f>", x, y);
+}
