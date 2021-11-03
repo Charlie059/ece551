dec98d58f9782a95cd39928191c5dc4a300876ce
xg73 <xg73@duke.edu>
2021-11-02 22:35:42 -0400

    083

---
+++ 083_expr_eval1/expr.hpp
@@ -0,0 +1,47 @@
+#ifndef __EXPR_HPP__
+#define __EXPR_HPP__
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+class Expression {
+ public:
+  virtual std::string toString() const = 0;
+  virtual ~Expression(){};
+};
+
+class NumExpression : public Expression {
+ private:
+  long num;
+
+ public:
+  NumExpression(long setNum) : num(setNum){};
+  std::string toString() const {
+    std::stringstream ss;
+    ss << this->num;
+    return ss.str();
+  }
+
+  virtual ~NumExpression(){};
+};
+
+class PlusExpression : public Expression {
+ private:
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : lhs(lhs), rhs(rhs){};
+  std::string toString() const {
+    std::stringstream ss;
+    ss << this->lhs->toString() << "+" << this->rhs->toString();
+    return ss.str();
+  };
+  virtual ~PlusExpression() {
+    delete this->lhs;
+    delete this->rhs;
+  };
+};
+
+#endif
