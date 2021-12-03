67e9ae47e572fa9b0ba5ab8b2c9247bce66a6e52
xg73 <xg73@duke.edu>
2021-12-02 22:52:52 -0500

    013

--- 093_eval3/Story.cpp
+++ 093_eval3/Story.cpp
@@ -151,7 +151,7 @@ int getUserInput(Page currentPage) {
       return selectionInt;
     }
     else {
-      std::cout << "That is not a valid choice, please try again";
+      std::cout << "That is not a valid choice, please try again\n";
       std::cin.clear();
     }
   } while (accept == false);
